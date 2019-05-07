c5c9a982900afd6428c44939711d31a4f901f1cf
sabbir-anwar <protik26@gmail.com>
2019-05-07 17:05:27 +0600

    basic landing page

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+// Menu Dropdown
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+// Sticky Header
+$(document).ready(function(){
+  "use strict";
+ if( $( window ).width() >= "768" ) {
+  $(".header-menu").sticky({topSpacing:0}); 
+  }
+});
+// flexslider
+$(window).load(function() {
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+ 
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: true,
+    sync: "#carousel"
+  });
+});
